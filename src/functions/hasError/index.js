ad5afd20f49f31539c8b0784ffc858597cdbafc5
dcouch440 <dcouch440@gmail.com>
2021-07-18 18:03:28 -0700

    create has errors

---
+++ src/functions/hasError/index.js
@@ -0,0 +1,16 @@
+/*
+  This functions is to check if an array of errors contains the of the input
+  // yup validation error
+  for example: [{ name: 'firstName', message: 'There was an error at First Name'}]
+
+  // there are other ways to retrieves name through the error itself but this way the error handling can come through one linier function system. For example, errors can store array of errors and can be mapped through safely as long as the writer of the yup error keeps a constant form
+
+*/
+
+const hasError = ({ errors, name }) => {
+  return errors.map(
+  error => error.name).filter(n => n === name
+  ).length !== 0
+}
+
+export default hasError;
