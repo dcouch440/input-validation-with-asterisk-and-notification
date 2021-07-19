7480d7ddda033cf49979aad4275013a2a773726c
dcouch440 <dcouch440@gmail.com>
2021-07-18 18:01:22 -0700

    add logic to display errors

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import AppTextInput from './components/AppTextInput'
+import { useState } from 'react'
+import ShowErrors from './components/ShowErrors'
+import AppSubmitButton from './components/AppSubmitButton'
+import * as yup from 'yup'
+import GlobalStyles from './GlobalStyles'
 
-function App() {
+export default function App () {
+  const [errors, setErrors] = useState([])
+  const [input, setInput] = useState({ testErrors: '' })
+
+  const exampleRules = ({ name, displayName }) => yup.object().shape({
+    [name]: yup
+      .string()
+      .required(() => ({
+        message: `There was an error at ${displayName}`,
+        name,
+      }))
+      .matches(/[0-9]/, () => ({
+        message: `Only numbers allowed at ${displayName}`,
+        name,
+      }))
+  })
+
+  const handleChange = ({ target: { name, value } }) => setInput(prev => ({
+    ...prev,
+    [name]: value
+  }))
+
+  const validate = async () => {
+    try {
+      await exampleRules({ name: 'testErrors', displayName: 'Test Errors' })
+        .validate(input)
+    } catch (err) {
+      console.log(err)
+      if (err.name === 'ValidationError') {
+        setErrors([...err.errors]);
+      }
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='global-container'>
+      <GlobalStyles />
+      <ShowErrors errors={errors} />
+      <AppTextInput
+        name='testErrors'
+        onChange={handleChange}
+        validatorErrors={errors}
+        title='Please Enter A Number'
+      />
+      <AppSubmitButton
+        onClick={validate}
+        text="Validate"
+      />
     </div>
   );
 }
-
-export default App;
