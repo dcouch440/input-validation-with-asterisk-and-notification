19af4425fe1ede38b7490d759a5744e05eb847d8
dcouch440 <dcouch440@gmail.com>
2021-07-20 20:59:28 -0700

    change to ts file

---
+++ src/functions/hasError/index.ts
@@ -0,0 +1,29 @@
+/*
+  This functions is to check if an array of errors contains the of the input
+  // yup validation error
+  for example: [{ name: 'firstName', message: 'There was an error at First Name'}]
+
+  // there are other ways to retrieves name through the error itself but this way the error handling can come through one linier function system. For example, errors can store array of errors and can be mapped through safely as long as the writer of the yup error keeps a constant form
+
+*/
+
+import {
+  IErrors
+} from "../../types";
+
+interface IHasError {
+  errors: IErrors[]
+  name: string
+}
+
+const hasError = (
+  { errors, name }:
+  IHasError
+) => {
+  return errors
+    .map((error) => error.name)
+    .filter(n => n === name)
+    .length !== 0
+}
+
+export default hasError;
