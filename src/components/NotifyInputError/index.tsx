753e11bbb7c3907995a1fc2a28b1e087ec1e3e0d
dcouch440 <dcouch440@gmail.com>
2021-07-22 02:42:37 -0700

    create comp

---
+++ src/components/NotifyInputError/index.tsx
@@ -0,0 +1,27 @@
+import hasError from '../../functions/hasError'
+import ErrorIcon from '@material-ui/icons/Error'
+
+import {
+  IErrors
+} from '../../types'
+
+interface INotifyInputError extends IErrors {
+  name: string
+  required: boolean
+}
+
+export default function NotifyInputError ({
+  validationErrors,
+  name,
+  required
+}: INotifyInputError): JSX.Element | null {
+  return (
+    hasError({ validationErrors, name }) ?
+      <ErrorIcon style={{ color: 'red' }} />
+      :
+      required ?
+        <span style={{ color: 'red', fontSize: '30px' }}> *</span>
+        :
+        null
+  )
+}
